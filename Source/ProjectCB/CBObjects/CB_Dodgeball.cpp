cbbfbb1b97e60b57dea7a23319d8fe1c41ac7907
He1i0s-OS <koschekajack836@gmail.com>
2020-10-11 23:49:35 -0700

    Map Updates
    made some updates to the map

--- Source/ProjectCB/CBObjects/CB_Dodgeball.cpp
+++ Source/ProjectCB/CBObjects/CB_Dodgeball.cpp
@@ -2,6 +2,7 @@
 
 
 #include "CB_Dodgeball.h"
+//#include "Classes/GameFramework/ProjectileMovementComponent.h"
 
 // Sets default values
 ACB_Dodgeball::ACB_Dodgeball()
@@ -9,14 +10,20 @@ ACB_Dodgeball::ACB_Dodgeball()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	this->m_mesh = CreateDefaultSubobject<UStaticMeshComponent>("Dodgeball");
+	this->m_mesh = CreateDefaultSubobject<UStaticMeshComponent>("DodgeballMesh");
+
+	SetRootComponent(this->m_mesh);
+
+	//this->m_movement = CreateDefaultSubobject<UProjectileMovementComponent>("DodgeballMovement");
+
+	//this->m_movement->InitialSpeed = this->m_speed;
+	//this->m_movement->MaxSpeed = this->m_speed;
 }
 
 // Called when the game starts or when spawned
 void ACB_Dodgeball::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
