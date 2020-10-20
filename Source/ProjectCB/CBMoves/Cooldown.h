16158caff466ec51818c22b32bf631a65551c36d
He1i0s-OS <koschekajack836@gmail.com>
2020-10-19 18:49:43 -0700

    Added Additional Classes
    added some additional classes for code clarity

---
+++ Source/ProjectCB/CBMoves/Cooldown.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+struct PROJECTCB_API Cooldown
+{
+	// TODO contains data pertaining to the cooldown of a move
+
+};
---
+++ Source/ProjectCB/CBMoves/Warmup.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+struct PROJECTCB_API Warmup
+{
+	// TODO contains data pertaining the warmup of a move
+};
