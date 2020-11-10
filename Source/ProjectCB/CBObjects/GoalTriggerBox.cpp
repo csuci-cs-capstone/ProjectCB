d63ed2e232cdfc870e02a96e764a58a837c20168
He1i0s-OS <koschekajack836@gmail.com>
2020-11-10 11:54:34 -0800

    Removed Debug Messages
    removed messages for goal debugging

--- Source/ProjectCB/CBObjects/GoalTriggerBox.cpp
+++ Source/ProjectCB/CBObjects/GoalTriggerBox.cpp
@@ -20,21 +20,12 @@ void AGoalTriggerBox::OnOverlapBegin(AActor* overlappedActor, AActor* otherActor
 {
 	if (otherActor && otherActor != this)
 	{
-		if (GEngine) // TODO remove
-			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, FString::Printf(TEXT("ENTERED")));
-
 		if (otherActor->IsA(ACB_DodgeballProjectile::StaticClass()))
 		{
-			// add ball to list
 			this->m_grabbableList.add(Cast<IGrabbable>(otherActor));
 			
 			// TODO make ball unable to move and roll towards center?
 		}
-		//else if (otherActor->IsA(ACB_PlayerCharacter::StaticClass()))
-		//{
-		//	// allow for ball to be picked up
-		//			// if grab is pressed, ball is picked up and removed from top of ball list if is any
-		//}
 	}
 }
 
@@ -42,24 +33,12 @@ void AGoalTriggerBox::OnOverlapEnd(AActor* overlappedActor, AActor* otherActor)
 {
 	if (otherActor && otherActor != this)
 	{
-		if (GEngine) // TODO remove
-			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, FString::Printf(TEXT("EXITED")));
-
-		// TODO make it so that...
-
-			// if otherActor is ball
-				// do nothing
-
 		if (otherActor->IsA(ACB_DodgeballProjectile::StaticClass()))
 		{
-			// remove ball from list
 			this->m_grabbableList.remove(Cast<IGrabbable>(otherActor));
 
 			// TODO make ball able to move again?
 		}
-
-			// else if player
-				// make unable to grab?
 	}
 }
 
