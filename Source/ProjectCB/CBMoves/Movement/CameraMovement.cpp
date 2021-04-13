ca64adcb56cef1afe7fd831e0a5f08b24f66d3ed
He1i0s-OS <koschekajack836@gmail.com>
2021-04-12 21:19:51 -0700

    Updated Camera
    updated camera controls and added camera sensitivity to pause menu

--- Source/ProjectCB/CBMoves/Movement/CameraMovement.cpp
+++ Source/ProjectCB/CBMoves/Movement/CameraMovement.cpp
@@ -1,16 +1,17 @@
 #include "CameraMovement.h"
 
 const FRotator CameraMovement::CAMERA_START_ROTATION(-20.0f, 0.0f, 0.0f);
-const float CameraMovement::CAMERA_TURN_SPEED = 1.0f * 2;
+const float CameraMovement::CAMERA_TURN_SPEED = 1.0f * 1.5f;
 const float CameraMovement::CAMERA_TURN_ACCELERATION = 1.0f / 8;
 
 CameraMovement::CameraMovement()
 {
 	this->m_cameraRotation = CameraMovement::CAMERA_START_ROTATION;
 	this->m_velocity = 0;
+	this->m_sensitivity = 1.0;
 }
 
-void CameraMovement::updateCamera(const float amount)
+void CameraMovement::updateCamera(Movement& movement, const float amount)
 {
 	if (amount == 0)
 		this->m_velocity = 0;
@@ -35,12 +36,19 @@ void CameraMovement::updateCamera(const float amount)
 			this->m_velocity = 1;
 	}
 
-	this->m_cameraRotation.Yaw += CameraMovement::CAMERA_TURN_SPEED * this->m_velocity;
+	this->m_cameraRotation.Yaw += this->m_sensitivity * CameraMovement::CAMERA_TURN_SPEED * this->m_velocity;
 
 	if (this->m_cameraRotation.Yaw >= 360)
 		this->m_cameraRotation.Yaw -= 360;
 	else if (this->m_cameraRotation.Yaw < 0)
 		this->m_cameraRotation.Yaw += 360;
+
+	movement.setCameraRotation(this->m_cameraRotation, this->m_velocity != 0);
+}
+
+void CameraMovement::setCameraSensitivity(float cameraSensitivity)
+{
+	this->m_sensitivity = cameraSensitivity;
 }
 
 const FRotator& CameraMovement::getCameraRotation()
